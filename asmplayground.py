f370b79f379006ad89bd846d86a42757f65c5355
Readm <assasaki@163.com>
2019-12-06 23:04:42 +0800

    building

--- asmplayground.py
+++ asmplayground.py
@@ -48,11 +48,13 @@ class Line(str):
     @property
     def is_directive(self): return self.type=='directive'
 
-    def get_directive_type(self): return self.strip().split()[0] if self.is_directive else None
+    def get_directive_type(self): return self.strip_comment().strip().split()[0] if self.is_directive else None
 
     @property
     def is_label(self): return self.type=='label'
 
+    def get_label(self): return self.strip_comment().strip().split()[0].replace(':','') if self.is_label else None
+    
     @property
     def is_section_directive(self):
         return True if self.is_directive and self.get_directive_type() in ['.section','.data','.text'] else False
@@ -63,8 +65,11 @@ class Line(str):
     
     @property
     def get_loc(self):
-        return [int(x) for x in self.split()[1:4]]
+        return ' '.join(self.split()[1:4])
     
+    def set_loc(self,loc):
+        self.debug_loc=loc
+
     @property
     def is_file_directive(self):
         return True if self.is_directive and self.get_directive_type() == '.file' else False
@@ -73,7 +78,6 @@ class Line(str):
     def is_debug_file_directive(self): # the .file has two version, old and DWARF2, we concern DWARF2 only.
         return True if self.is_file_directive and not self.strip_comment().split()[1].startswith('"') else False
     
-    @property
     def set_section_declaration(self,line):
         self.section_declaration=line
 
@@ -81,29 +85,62 @@ class AsmSrc(str):
     def __init__(self,s):
         super(AsmSrc, self).__init__()
         self.lines=[Line(i) for i in self.split('\n')]
+        self.labels=dict()
 
-        self.debug_file=dict() # key: file value: number
+        self.debug_file_number=dict() # key: file value: number
         current_section=None
+        current_loc=None
         for line in self.lines:
             # update file number
             if line.is_debug_file_directive: 
                 file_num,file_str = line.split()[1], line.split()[2]
-                self.debug_file[file_str.replace('"','')]=int(file_num)
+                self.debug_file_number[file_str.replace('"','')]=int(file_num)
+
             if line.is_section_directive:
                 current_section=line
             elif line.is_instruction:
                 line.set_section_declaration(current_section)
+            
+            if line.is_label:
+                self.labels[line.get_label()]=line
+            
+            if line.is_loc_directive:
+                current_loc=line.get_loc
+            elif line.is_instruction:
+                line.set_loc(current_loc)
+
+    def update_debug_file_number(self,path):
+        # we add more keys to the debug_file_number to facilitate the mapping
+        for key in [k for k in self.debug_file_number.keys()]:
+            new_key = key.replace(path,'')
+            self.debug_file_number[new_key]=self.debug_file_number[key]
+        for key in [k for k in self.debug_file_number.keys()]:
+            if key.startswith('.'):
+                new_key = key.replace('.','')
+                self.debug_file_number[new_key]=self.debug_file_number[key]
+
+        
+    def get_file_numbers(self):
+        return self.debug_file_number
+        
+    def find_label(self, label):
+        try:
+            return self.labels[label]
+        except KeyError:
+            return None
+            # raise Exception('Label %d not found' % label)
+        
 
     @classmethod
-    def read_file(cls,path):
+    def read_file(cls,path,src_path=''):
         with open(path) as f:
-            return cls(f.read())
+            asm = cls(f.read())
+            asm.update_debug_file_number(src_path)
+            return asm
 
 
 
 if __name__=="__main__":
     asm=AsmSrc.read_file('./testcase/401.bzip.s')
     for line in asm.lines:
-        if line.is_debug_file_directive: print(line)
-    print(asm.debug_file)
-        # print(line.type,line)# == 'instruction': print(line)
+        if line.is_instruction and 'call' in line: print(line)
--- scif.py
+++ scif.py
@@ -1,3 +1,98 @@
+from pprint import pprint
 from asmplayground import *
 
-class 
+def is_indirect_call(line,language='x86_at&t'):
+    # remember to add rules for more languages
+    return True if line.is_instruction and 'call' in line and '*' in line else False
+
+#TODO: support indirect jump
+def is_indirect_jump(line,language='x86_at&t'):
+    return False
+
+def is_indirect_branch(line,language='x86_at&t'):
+    return is_indirect_call(line,language='x86_at&t') or is_indirect_jump(line,language='x86_at&t')
+
+class CFG():
+    def __init__(self, target=dict(), branch=dict()):
+        self.target=target # label-> [tags]
+        self.branch=branch # debug loc -> [tags]
+        # remember: the tags is in a list, we support multi tags
+    
+    @classmethod
+    def read_from_llvm(cls,path):
+        with open(path) as f:
+            target, branch = dict(), dict()
+            for line in f:
+                line = line.strip()
+                if line.startswith('FastCFI:callee'):
+                    info=line.replace('FastCFI:callee=','').split('@')
+                    target[info[0]]=[info[-1]]
+                elif line.startswith('FastCFI:caller'):
+                    info=line.replace('FastCFI:caller=','').split('@')
+                    branch[info[0]]=[info[-1]]
+            valid_tags=branch.values()
+            for key in [k for k in target.keys()]:
+                if target[key] not in valid_tags:
+                    target.pop(key)
+            return cls(target=target, branch=branch)
+            
+    
+    # convert the string:y:z to x y z form
+    def convert_filename_to_number(self, file_numbers):
+        for branch_loc in [k for k in self.branch.keys()]:
+            new_key = str(file_numbers[branch_loc.split(':')[0]])
+            new_key += ' '+' '.join(branch_loc.split(':')[1:3])
+            self.branch[new_key]=self.branch[branch_loc]
+            self.branch.pop(branch_loc)
+
+
+
+class SCFIAsm(AsmSrc):
+    def __init__(self, s, cfg=CFG(), src_path=''):
+        super().__init__(s)
+        self.cfg=cfg
+        self.slot_bit_width=8
+        self.branch_lst=[]
+        self.marked_branch_lst=[]
+        self.marked_target_lst=[]
+        self.update_debug_file_number(src_path)
+
+    @property
+    def cfi_info(self):
+        info = \
+            'slot bit width: %d\n' % self.slot_bit_width+\
+            'icalls: %d\n' % len(self.branch_lst) +\
+            'marked_icalls: %d\n' % len(self.marked_branch_lst) +\
+            'marked_targets: %d\n' % len(self.marked_target_lst) +\
+            'cfg_branches: %d\n' % len(self.cfg.branch.keys()) +\
+            'cfg_targets: %d\n' % len(self.cfg.target.keys())
+        return info
+
+    
+    def prepare_and_count(self):
+        for line in self.lines:
+            if is_indirect_branch(line): 
+                self.branch_lst.append(line)
+
+    def mark_all_instructions(self,cfg=None):
+        if cfg: self.cfg=cfg
+        self.cfg.convert_filename_to_number(self.debug_file_number)
+        self.mark_all_branches()
+        self.mark_all_targets()
+    def mark_all_branches(self):
+        for branch in self.branch_lst:
+            if branch.debug_loc in self.cfg.branch.keys():
+                setattr(branch,'tag',self.cfg.branch[branch.debug_loc])
+                self.marked_branch_lst.append(branch)
+    def mark_all_targets(self):
+        for label in self.cfg.target.keys():
+            if self.find_label(label):
+                setattr(self.find_label(label),'tags',self.cfg.target[label])
+                self.marked_target_lst.append(self.find_label(label))
+    
+
+if __name__ == '__main__':
+    asm=SCFIAsm.read_file('./testcase/401.bzip.s',src_path='/home/readm/fast-cfi/401.bzip2/')
+    asm.prepare_and_count()
+    asm.mark_all_instructions(cfg=CFG.read_from_llvm('./testcase/cfg.txt'))
+    print(asm.cfi_info)
