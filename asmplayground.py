7ee600223eeacfac5ead24f5c1efe1480afd9a1b
Readm <assasaki@163.com>
2019-12-07 21:56:00 +0800

    pep8 format

--- asmplayground.py
+++ asmplayground.py
@@ -3,164 +3,205 @@
 #      Add line before/after a line
 #      Move lines to a new location
 
-#TODO:
+import logging
+import subprocess
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('SCFI')
+
+# TODO:
 # Move lines: cut into functions
 #             cut function into basic blocks
 # Move all file declaration to the begining
 # Insert line
 
+
 class Line(str):
-    key_id = 0  # we need a unique id for each Line, otherwise we cannot distiguish different line with the same content
-    def __init__(self,s):
+    key_id = 0  # we need a unique id for each Line, otherwise we cannot distinguish different line with the same content
+
+    def __init__(self, s):
         super(Line, self).__init__()
         self._key_id = Line.key_id
-        Line.key_id +=1
+        Line.key_id += 1
         # self.type: 'empty' 'instruction' 'comment' 'directive' 'label'
 
-        #TODO: we assume no /*...*/ comments, or we should remove them in AsmSrc.__init__
-        if "/*" in self: raise Exception('Not supported comment')
+        # TODO: we assume no /*...*/ comments, or we should remove them in AsmSrc.__init__
+        if "/*" in self:
+            raise Exception('Not supported comment')
 
         first_char = self.lstrip()[:1]
         last_char = self.strip_comment().rstrip()[-1:]
         if not first_char:
             self.type = 'empty'
-        elif first_char=='#':
+        elif first_char == '#':
             self.type = 'comment'
-        elif last_char==':':
+        elif last_char == ':':
             self.type = 'label'
-        elif first_char=='.':
+        elif first_char == '.':
             self.type = 'directive'
         else:
             self.type = 'instruction'
-        
-        self.section_declaration=None
-        self.moved=False
+
+        self.section_declaration = None
+        self.moved = False
 
     def __eq__(self, other):
         if isinstance(other, Line):
             return other and self._key_id == other._key_id
         else:
-            return super(Line,self).__eq__(other)
-        
+            return super(Line, self).__eq__(other)
 
     def strip_comment(self):
         return self.split('#')[0]
-    
-    @property
-    def is_empty(self): return self.type=='empty'
-    
-    @property
-    def is_instruction(self): return self.type=='instruction'
 
     @property
-    def is_comment(self): return self.type=='comment'
+    def is_empty(self): return self.type == 'empty'
 
     @property
-    def is_directive(self): return self.type=='directive'
-
-    def get_directive_type(self): return self.strip_comment().strip().split()[0] if self.is_directive else None
+    def is_instruction(self): return self.type == 'instruction'
 
     @property
-    def is_label(self): return self.type=='label'
+    def is_comment(self): return self.type == 'comment'
 
-    def get_label(self): return self.strip_comment().strip().split()[0].replace(':','') if self.is_label else None
-    
+    @property
+    def is_directive(self): return self.type == 'directive'
+
+    def get_directive_type(self): return self.strip_comment().strip().split()[
+        0] if self.is_directive else None
+
+    @property
+    def is_label(self): return self.type == 'label'
+
+    def get_label(self): return self.strip_comment().strip().split()[
+        0].replace(':', '') if self.is_label else None
+
     @property
     def is_section_directive(self):
-        return True if self.is_directive and self.get_directive_type() in ['.section','.data','.text'] else False
-    
+        return True if self.is_directive and self.get_directive_type() in ['.section', '.data', '.text'] else False
+
     @property
     def is_loc_directive(self):
         return True if self.is_directive and self.get_directive_type() == '.loc' else False
-    
+
     @property
     def get_loc(self):
         return ' '.join(self.split()[1:4])
-    
-    def set_loc(self,loc):
-        self.debug_loc=loc
+
+    def set_loc(self, loc):
+        self.debug_loc = loc
 
     @property
     def is_file_directive(self):
         return True if self.is_directive and self.get_directive_type() == '.file' else False
-    
+
     @property
-    def is_debug_file_directive(self): # the .file has two version, old and DWARF2, we concern DWARF2 only.
+    # the .file has two version, old and DWARF2, we concern DWARF2 only.
+    def is_debug_file_directive(self):
         return True if self.is_file_directive and not self.strip_comment().split()[1].startswith('"') else False
-    
-    def set_section_declaration(self,line):
-        self.section_declaration=line
+
+    def set_section_declaration(self, line):
+        self.section_declaration = line
+
 
 class AsmSrc(str):
-    def __init__(self,s):
+    def __init__(self, s):
         super(AsmSrc, self).__init__()
-        self.lines=[Line(i) for i in self.split('\n')]
-        self.labels=dict()
+        self.lines = [Line(i) for i in self.split('\n')]
+        self.labels = dict()
 
-        self.debug_file_number=dict() # key: file value: number
-        current_section=None
-        current_loc=None
+        self.debug_file_number = dict()  # key: file value: number
+        current_section = None
+        current_loc = None
         for line in self.lines:
             # update file number
-            if line.is_debug_file_directive: 
-                file_num,file_str = line.split()[1], line.split()[2]
-                self.debug_file_number[file_str.replace('"','')]=int(file_num)
+            if line.is_debug_file_directive:
+                file_num, file_str = line.split()[1], line.split()[2]
+                self.debug_file_number[file_str.replace(
+                    '"', '')] = int(file_num)
 
             if line.is_section_directive:
-                current_section=line
+                current_section = line
             elif line.is_instruction:
                 line.set_section_declaration(current_section)
-            
+
             if line.is_label:
-                self.labels[line.get_label()]=line
-            
+                self.labels[line.get_label()] = line
+
             if line.is_loc_directive:
-                current_loc=line.get_loc
+                current_loc = line.get_loc
             elif line.is_instruction:
                 line.set_loc(current_loc)
-    
+
     def __str__(self):
         return ''.join([self.lines])
 
-    def update_debug_file_number(self,path):
+    def update_debug_file_number(self, path):
         # we add more keys to the debug_file_number to facilitate the mapping
-        if not path: return
+        if not path:
+            return
         for key in [k for k in self.debug_file_number.keys()]:
-            new_key = key.replace(path,'')
-            self.debug_file_number[new_key]=self.debug_file_number[key]
+            new_key = key.replace(path, '')
+            self.debug_file_number[new_key] = self.debug_file_number[key]
         for key in [k for k in self.debug_file_number.keys()]:
             if key.startswith('.'):
-                new_key = key.replace('.','')
-                self.debug_file_number[new_key]=self.debug_file_number[key]
+                new_key = key.replace('.', '')
+                self.debug_file_number[new_key] = self.debug_file_number[key]
 
-        
     def get_file_numbers(self):
         return self.debug_file_number
-        
+
     def find_label(self, label):
         try:
             return self.labels[label]
         except KeyError:
+            logger.warn('label %s not found' % label)
             return None
-            # raise Exception('Label %d not found' % label)
-    
+
     def index_of_line(self, line):
         return self.lines.index(line)
 
     def insert_before(self, insert_line, before_line):
         self.lines.insert(self.index_of_line(before_line), insert_line)
-    
+
     def insert_after(self, insert_line, after_line):
         self.lines.insert(self.index_of_line(after_line)+1, insert_line)
 
+    # TODO: make moves faster: it calls index_of_line too many times
+    def move_lines_before(self, line_lst, before_line):
+        for line in line_lst:
+            self.lines.remove(line)
+        for line in line_lst:
+            self.insert_before(line, before_line)
+
+    def move_lines_after(self, line_lst, after_line):
+        for line in line_lst:
+            self.lines.remove(line)
+        for line in line_lst:
+            self.insert_after(line, after_line)
+
+    def move_file_directives_forward(self):
+        # move so that all .loc will not meet undeclared file
+        last_index = 0
+        for i in range(len(self.lines)):
+            if self.lines[i].is_debug_file_directive:
+                if not last_index:
+                    last_index = i
+                    continue
+                if i > last_index+1:
+                    self.lines.insert(last_index+1, self.lines.pop(i))
+                last_index += 1
+        pass
+
     @classmethod
-    def read_file(cls,path,src_path=''):
+    def read_file(cls, path, src_path=''):
         with open(path) as f:
             asm = cls(f.read())
             asm.update_debug_file_number(src_path)
             return asm
 
-if __name__=="__main__":
-    asm=AsmSrc.read_file('./testcase/401.bzip.s')
+
+if __name__ == "__main__":
+    asm = AsmSrc.read_file('./testcase/401.bzip.s')
     for line in asm.lines:
-        if line.is_instruction and 'call' in line: print(line)
+        if line.is_instruction and 'call' in line:
+            print(line)
--- scif.py
+++ scif.py
@@ -1,173 +1,182 @@
+from asmplayground import *
 from pprint import pprint
-import logging,subprocess
-logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+import logging
+import subprocess
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('SCFI')
 
 
-from asmplayground import *
-
-def is_indirect_call(line,language='x86_at&t'):
+def is_indirect_call(line, language='x86_at&t'):
     # remember to add rules for more languages
     return True if line.is_instruction and 'call' in line and '*' in line else False
 
-#TODO: support indirect jump
-def is_indirect_jump(line,language='x86_at&t'):
+# TODO: support indirect jump
+
+
+def is_indirect_jump(line, language='x86_at&t'):
     return False
 
-def is_indirect_branch(line,language='x86_at&t'):
-    return is_indirect_call(line,language='x86_at&t') or is_indirect_jump(line,language='x86_at&t')
+
+def is_indirect_branch(line, language='x86_at&t'):
+    return is_indirect_call(line, language='x86_at&t') or is_indirect_jump(line, language='x86_at&t')
 
 # retrun a Line of .org
-def padding_to_slot(bit_width, slot):
-    return Line('\t.org ((.-0x%x-1)/(1<<%d)+1)*(1<<%d)+0x%x, 0x90 \t# pad to 0x%x, in width %d\n' %\
-        (slot, bit_width, bit_width, slot, slot, bit_width))
 
-def padding_to_label(bit_width,label):
-    return Line('\t.org ((.-(%s%%(1<<%d))-1)/(1<<%d)+1)*(1<<%d)+(%s%%(1<<%d)), 0x90 \t# pad to %s, in width %d\n' % \
-        (label, bit_width, bit_width, bit_width, label ,bit_width, label, bit_width))
+
+def padding_to_slot(bit_width, slot):
+    return Line('\t.org ((.-0x%x-1)/(1<<%d)+1)*(1<<%d)+0x%x, 0x90 \t# pad to 0x%x, in width %d\n' %
+                (slot, bit_width, bit_width, slot, slot, bit_width))
+
+
+def padding_to_label(bit_width, label):
+    return Line('\t.org ((.-(%s%%(1<<%d))-1)/(1<<%d)+1)*(1<<%d)+(%s%%(1<<%d)), 0x90 \t# pad to %s, in width %d\n' %
+                (label, bit_width, bit_width, bit_width, label, bit_width, label, bit_width))
 
 # Label based CFG, each target/branch has tags(labels)
-# Tags canbe strings, int ...
+# Tags can be strings, int ...
 # For each target: keyed by label
 # For each branch: keyed by debug_loc
 # add new read function for new formats
+
+
 class CFG():
     def __init__(self, target=dict(), branch=dict()):
-        self.target=target # label-> [tags]
-        self.branch=branch # debug loc -> [tags]
+        self.target = target  # label-> [tags]
+        self.branch = branch  # debug loc -> [tags]
         # remember: the tags is in a list, we support multi tags
-    
+
     @classmethod
-    def read_from_llvm(cls,path):
+    def read_from_llvm(cls, path):
         with open(path) as f:
             target, branch = dict(), dict()
             for line in f:
                 line = line.strip()
                 if line.startswith('FastCFI:callee'):
-                    info=line.replace('FastCFI:callee=','').split('@')
-                    target[info[0]]=[info[-1]]
+                    info = line.replace('FastCFI:callee=', '').split('@')
+                    target[info[0]] = [info[-1]]
                 elif line.startswith('FastCFI:caller'):
-                    info=line.replace('FastCFI:caller=','').split('@')
-                    branch[info[0]]=[info[-1]]
-            valid_tags=branch.values()
+                    info = line.replace('FastCFI:caller=', '').split('@')
+                    branch[info[0]] = [info[-1]]
+            valid_tags = branch.values()
             for key in [k for k in target.keys()]:
                 if target[key] not in valid_tags:
                     target.pop(key)
             return cls(target=target, branch=branch)
-            
-    
+
     # convert the string:y:z to x y z form
+
     def convert_filename_to_number(self, file_numbers):
         for branch_loc in [k for k in self.branch.keys()]:
             new_key = str(file_numbers[branch_loc.split(':')[0]])
             new_key += ' '+' '.join(branch_loc.split(':')[1:3])
-            self.branch[new_key]=self.branch[branch_loc]
+            self.branch[new_key] = self.branch[branch_loc]
             self.branch.pop(branch_loc)
 
 
 class SCFIAsm(AsmSrc):
     def __init__(self, s, cfg=CFG(), src_path=''):
         super().__init__(s)
-        self.cfg=cfg
-        self.slot_bit_width=8
-        self.branch_lst=[]
-        self.marked_branch_lst=[]
-        self.marked_target_lst=[]
-        self.valid_tags=set()       # cfg contains more tags than our object
-        self.tag_slot=dict()
+        self.cfg = cfg
+        self.slot_bit_width = 8
+        self.branch_lst = []
+        self.marked_branch_lst = []
+        self.marked_target_lst = []
+        self.valid_tags = set()       # cfg contains more tags than our object
+        self.tag_slot = dict()
 
-        self.tmp_asm_path='/tmp/scfi_tmp.s'
-        self.tmp_obj_path='/tmp/scfi_tmp.o'
-        self.tmp_dmp_path='/tmp/scfi_tmp.dump'
+        self.tmp_asm_path = '/tmp/scfi_tmp.s'
+        self.tmp_obj_path = '/tmp/scfi_tmp.o'
+        self.tmp_dmp_path = '/tmp/scfi_tmp.dump'
 
         self.update_debug_file_number(src_path)
 
-    @property
-    def cfi_info(self):
-        info = \
-            'slot bit width: %d\n' % self.slot_bit_width+\
-            'icalls: %d\n' % len(self.branch_lst) +\
-            'marked_icalls: %d\n' % len(self.marked_branch_lst) +\
-            'marked_targets: %d\n' % len(self.marked_target_lst) +\
-            'cfg_branches: %d\n' % len(self.cfg.branch.keys()) +\
-            'cfg_targets: %d\n' % len(self.cfg.target.keys())
-        return info
-
-    
     def prepare_and_count(self):
         for line in self.lines:
-            if is_indirect_branch(line): 
+            if is_indirect_branch(line):
                 self.branch_lst.append(line)
 
-    def mark_all_instructions(self,cfg=None):
-        if cfg: self.cfg=cfg
+    def mark_all_instructions(self, cfg=None):
+        if cfg:
+            self.cfg = cfg
         self.cfg.convert_filename_to_number(self.debug_file_number)
         self.mark_all_branches()
         self.mark_all_targets()
         logger.info('marked all instructions')
         logger.info('slot bit width: %d' % self.slot_bit_width)
-        logger.info('icalls: %d' % len(self.branch_lst) )
-        logger.info('marked_icalls: %d' % len(self.marked_branch_lst) )
-        logger.info('marked_targets: %d' % len(self.marked_target_lst) )
-        logger.info('cfg_branches: %d' % len(self.cfg.branch.keys()) )
+        logger.info('icalls: %d' % len(self.branch_lst))
+        logger.info('marked_icalls: %d' % len(self.marked_branch_lst))
+        logger.info('marked_targets: %d' % len(self.marked_target_lst))
+        logger.info('cfg_branches: %d' % len(self.cfg.branch.keys()))
         logger.info('cfg_targets: %d' % len(self.cfg.target.keys()))
         logger.info('valid tags: %d' % len(self.valid_tags))
+
     def mark_all_branches(self):
         for branch in self.branch_lst:
             if branch.debug_loc in self.cfg.branch.keys():
-                setattr(branch,'tags',self.cfg.branch[branch.debug_loc])
+                setattr(branch, 'tags', self.cfg.branch[branch.debug_loc])
                 self.marked_branch_lst.append(branch)
-                for tag in self.cfg.branch[branch.debug_loc]: self.valid_tags.add(tag)
+                for tag in self.cfg.branch[branch.debug_loc]:
+                    self.valid_tags.add(tag)
+
     def mark_all_targets(self):
         for label in self.cfg.target.keys():
             if self.find_label(label):
-                setattr(self.find_label(label),'tags',self.cfg.target[label])
+                setattr(self.find_label(label), 'tags', self.cfg.target[label])
                 self.marked_target_lst.append(self.find_label(label))
-                for tag in self.cfg.target[label]: self.valid_tags.add(tag)
-    
+                for tag in self.cfg.target[label]:
+                    self.valid_tags.add(tag)
+
     def random_slot_allocation(self):
         # use hash, this allocation requires no compile
         for target in self.marked_target_lst:
             slots = []
             for tag in target.tags:
-                slot = hash(tag) & ((1<<self.slot_bit_width)-1)
-                self.tag_slot[tag]=slot
+                slot = hash(tag) & ((1 << self.slot_bit_width)-1)
+                self.tag_slot[tag] = slot
                 slots.append(slot)
             setattr(target, 'slots', slots)
         for branch in self.marked_branch_lst:
             slots = []
             for tag in branch.tags:
-                slot = hash(tag) & ((1<<self.slot_bit_width)-1)
-                self.tag_slot[tag]=slot
+                slot = hash(tag) & ((1 << self.slot_bit_width)-1)
+                self.tag_slot[tag] = slot
                 slots.append(slot)
             setattr(target, 'slots', slots)
         for tag in self.tag_slot.keys():
-            logger.debug('random_allocation:\t'+str(tag)+' -> \t'+hex(self.tag_slot[tag]))
-    
+            logger.debug('random_allocation:\t'+str(tag) +
+                         ' -> \t'+hex(self.tag_slot[tag]))
+
     def compile_tmp(self, cmd=''):
         logger.info('compiling...')
         with open(self.tmp_asm_path, 'w') as f:
             f.write(self)
         if not cmd:
             cmd = 'as %s -o %s' % (self.tmp_asm_path, self.tmp_obj_path)
-        p=subprocess.Popen(cmd,stderr=subprocess.PIPE,shell=True)
+        p = subprocess.Popen(cmd, stderr=subprocess.PIPE, shell=True)
         p.wait()
-        compile_err=p.stderr.read()
-        if compile_err:raise Exception(compile_err)
+        compile_err = p.stderr.read()
+        if compile_err:
+            raise Exception(compile_err)
         c = 'objdump -d %s > %s' % (self.tmp_obj_path, self.tmp_dmp_path)
-        p=subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
         p.wait()
         logger.info('Finish compile.')
 
 
 if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
-    asm=SCFIAsm.read_file('./testcase/401.bzip.s',src_path='/home/readm/fast-cfi/401.bzip2/')
+    asm = SCFIAsm.read_file('./testcase/401.bzip.s',
+                            src_path='/home/readm/fast-cfi/401.bzip2/')
     asm.prepare_and_count()
     asm.mark_all_instructions(cfg=CFG.read_from_llvm('./testcase/cfg.txt'))
     asm.random_slot_allocation()
-    #print(asm.cfi_info)
+    asm.move_file_directives_forward()
     for line in asm.lines:
-        if '.text' in line:
-            pprint(line)
-            pprint(asm.index_of_line(line))
+        if line.is_debug_file_directive:
+            print(line)
+    # print(asm.cfi_info)
+    # for line in asm.lines:
+    #     if '.text' in line:
+    #         pprint(line)
+    #         pprint(asm.index_of_line(line))
